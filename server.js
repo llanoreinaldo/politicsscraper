c8348978df77ed63910fb4e8343b83db457a8604
Reinaldo Llano <llanoreinaldo@gmail.com>
2018-06-22 10:50:48 -0400

    Commit initial files.

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-
+var path = require('path');
 var axios = require("axios");
 var cheerio = require("cheerio");
 
 //Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = orocess.env.PORT || 3307;
 
 // Initialize Express
 var app = express();
@@ -22,7 +22,14 @@ app.use(logger("dev"));
 //Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true}));
 //Use express.static to server the public folder as a static directory
-app.use(express.static("public"));
+app.use(express.static("public/"));
+
+//Handlebars
+const exphndlbrs = require("express-handlebars");
+app.engine("handlebars", exphndlbrs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
 
 //Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
