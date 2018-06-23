b44239592238e690731716c66f5113b92d0c6b19
Reinaldo Llano <llanoreinaldo@gmail.com>
2018-06-22 23:05:05 -0400

    commit additional files and changes

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 //Initialize required node.js dependencies 
-var express = require("express");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-var path = require('path');
-var axios = require("axios");
-var cheerio = require("cheerio");
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require('path');
+const axios = require("axios");
+const cheerio = require("cheerio");
+const htmlRouter = require('./routes/htmlRoutes.js')
+const apiRouter = require('./routes/apiRoutes.js')
 
 //Require all models
 var db = require("./models");
 
-var PORT = orocess.env.PORT || 3307;
+var PORT = process.env.PORT || 3307;
+
 
 // Initialize Express
 var app = express();
 
+
 //Configure middleware
+
 
 //Use morgan logger for logging requests
 app.use(logger("dev"));
@@ -23,6 +28,9 @@ app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: true}));
 //Use express.static to server the public folder as a static directory
 app.use(express.static("public/"));
+app.use('/', htmlRouter);
+app.use('/api', apiRouter);
+
 
 //Handlebars
 const exphndlbrs = require("express-handlebars");
@@ -30,6 +38,7 @@ app.engine("handlebars", exphndlbrs({
     defaultLayout: "main"
 }));
 app.set("view engine", "handlebars");
+
 
 //Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
